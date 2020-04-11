54a4d7aeaad41737a6a7e3110ddfd31a0239de97
kote44 <kotekote0404@gmail.com>
2020-04-11 16:32:18 +0300

    Added initial UsersList, updated configuration

--- src/tests/main/App.test.tsx
+++ src/tests/main/App.test.tsx
@@ -4,9 +4,9 @@ import { shallow } from 'enzyme'
 import { shallowToJson } from 'enzyme-to-json'
 
 // Tested component
-import App from 'modules/main/App'
+import App from 'main/App'
 
-describe('modules/main/App component', () => {
+describe('main/App component', () => {
   it('should render correctly', () => {
     const component = shallow(<App />)
 
