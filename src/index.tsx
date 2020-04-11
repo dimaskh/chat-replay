bb6cd3df6d9b8e1249350f8053b93f2c8518da22
kote44 <kotekote0404@gmail.com>
2020-04-11 07:04:56 +0300

    Added prettier, eslint, updated files structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,24 @@
 // Libs
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 // Components
-import App from './App';
+import App from 'modules/main/App'
 
 // Styles
-import './styles/normalize.css';
+import 'styles/normalize.css'
 
 // Utils
-import * as serviceWorker from './utils/serviceWorker';
+import * as serviceWorker from 'utils/serviceWorker'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
